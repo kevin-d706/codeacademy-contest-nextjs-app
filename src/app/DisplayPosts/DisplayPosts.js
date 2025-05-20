477b167808fb72c6c18cb54733a522960c66e565
Kevin Dudeja <kevindudeja97@gmail.com>
2025-05-21 01:11:46 +0530

    Update displayposts.js for mobile resposiveness

--- src/app/DisplayPosts/DisplayPosts.js
+++ src/app/DisplayPosts/DisplayPosts.js
@@ -1,6 +1,8 @@
 "use client";
 import React from 'react';
 import '../styles/globals.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import Card from 'react-bootstrap/Card';
 
@@ -8,25 +10,25 @@ export function DisplayPosts({
     onReadPost,
     filteredPosts
 }) {
-    
+
 
     return (
         <>
             <h2>Blog Posts</h2>
-            <div className='container d-flex flex-wrap justify-start gap-5 p-0 m-0'>
+            <Row className="g-3">
                 {filteredPosts.map((post) => (
-                    <Card key={post.id || post.slug} style={{ width: '18rem' }}>
-                        {post.img && <Card.Img variant="top" src={post.img} style={{ padding: 10, borderRadius: 15 }} />}
-                        <Card.Body>
-                            <Card.Title>{post.title}</Card.Title>
-                            <Card.Text>
-                                {post.description}
-                            </Card.Text>
-                            <button onClick={() => onReadPost(post.slug)} className='btn-theme px-2 py-1 rounded'>Read More ➝</button>
-                        </Card.Body>
-                    </Card>
+                    <Col key={post.id || post.slug} xs={12} sm={6} md={4} lg={3}>
+                        <Card className="h-100 d-flex flex-column">
+                            {post.img && <Card.Img variant="top" src={post.img} style={{ padding: 10, borderRadius: 15 }} />}
+                            <Card.Body className="d-flex flex-column">
+                                <Card.Title>{post.title}</Card.Title>
+                                <Card.Text>{post.description}</Card.Text>
+                                <button onClick={() => onReadPost(post.slug)} className='btn-theme px-2 py-1 rounded mt-auto'>Read More ➝</button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 ))}
-            </div>
+            </Row>
         </>
 
 
